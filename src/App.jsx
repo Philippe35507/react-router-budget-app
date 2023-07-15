ab862e03bdb763e5ab0263668b4f2a21263cbece
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-15 20:32:04 +0100

    createBrowserRouter + RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hi</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 function App() {
-  return <div className="app">Setup</div>;
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
