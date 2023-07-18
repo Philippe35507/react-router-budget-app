b9788d3fa9ac584622d0f69e90c2086ab4840bbf
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-18 13:39:26 +0100

    Custom Error Page 2/2
    useNavigate() Hook for Go back button

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,17 @@
-import { Link, useRouteError } from "react-router-dom";
+import { Link, useNavigate, useRouteError } from "react-router-dom";
 
 //library imports
 import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
 
 const Error = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
   return (
     <div className="error">
       <h1>oh oh ... We've got a problem.</h1>
       <p>{error.message || error.statusText}</p>
       <div className="flex-md">
-        <button className="btn btn--dark">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
           <ArrowUturnLeftIcon width={20} />
           <span>Go back</span>
         </button>
