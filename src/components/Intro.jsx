b699c1cfd282d3a8173fcfd25328a1c3ccb4d045
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-18 13:27:41 +0100

    Adding a Login Form
    Form + function dashboardAction + <Intro.jsx /> + conditional rendering

---
+++ src/components/Intro.jsx
@@ -0,0 +1,41 @@
+// rrd import
+import { Form } from "react-router-dom";
+
+// library
+import { UserPlusIcon } from "@heroicons/react/24/solid";
+
+// assets
+import illustration from "../assets/illustration.jpg";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div>
+        <h1>
+          Take Control of <span className="accent">Your Money</span>
+        </h1>
+        <p>
+          Personal budgeting is the secret for personal freedom. Start you
+          journey today.
+        </p>
+        <Form method="post">
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="What is your name?"
+            arial-label="Your Name"
+            autoComplete="given-name"
+          />
+          <button type="submit" className="btn btn--dark">
+            <span>Create acount</span>
+            <UserPlusIcon> width={20}</UserPlusIcon>
+          </button>
+        </Form>
+      </div>
+      <img src={illustration} alt="Person with money" width={600} />
+    </div>
+  );
+};
+
+export default Intro;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
 // rrd imports
 import { useLoaderData } from "react-router-dom";
 
+// components
+import Intro from "../components/Intro";
+
 // helper functions
 import { fetchData } from "../helpers";
+
+// library
+import { toast } from "react-toastify";
 
 // loader
 export function dashboardLoader() {
@@ -10,14 +16,21 @@ export function dashboardLoader() {
   return { userName };
 }
 
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData();
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 
 export default Dashboard;
