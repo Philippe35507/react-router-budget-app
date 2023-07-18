0f1276252e0526f859d6fadaf123e84142cf55c8
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-18 13:35:41 +0100

    Custom Error Page 1/2
    useRouteError() Hook

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,26 @@
+import { Link, useRouteError } from "react-router-dom";
+
+//library imports
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  return (
+    <div className="error">
+      <h1>oh oh ... We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark">
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
